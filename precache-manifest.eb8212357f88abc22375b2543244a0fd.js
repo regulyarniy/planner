8c62ba1522912794f5ee47da65ea20753acfd971
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-02-12 07:05:46 +0000

    Deploy regulyarniy/planner to github.com/regulyarniy/planner.git:gh-pages

---
+++ precache-manifest.eb8212357f88abc22375b2543244a0fd.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "2f1323aa24d6521be717",
+    "url": "/static/js/main.2f1323aa.chunk.js"
+  },
+  {
+    "revision": "0f0f1ed395de9ee32ca5",
+    "url": "/static/js/2.0f0f1ed3.chunk.js"
+  },
+  {
+    "revision": "797adb734e823a488b43dc26db51af96",
+    "url": "/index.html"
+  }
+];
