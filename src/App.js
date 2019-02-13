a577ed6d50ec3e109c149f9b1ad0b0004c388fef
Regulyarniy Nikolay <regularniy@gmail.com>
2019-02-13 16:02:21 +1000

    adding styles

--- src/App.js
+++ src/App.js
@@ -24,13 +24,13 @@ class App extends Component {
                 </div>
               </header>
               <section className="step stage__step">
-                <h3 className="step__title">Шаг №1</h3>
+                <h3 className="step__title">Шаг №111111</h3>
                 <p className="step__time">
                   <span>Время</span>
                   <b>00:00:00</b>
                 </p>
-                <button className="element step__element" type="button">#130</button>
-                <button className="element step__element" type="button">#130</button>
+                <button className="element step__element" type="button"><span>#130</span></button>
+                <button className="element step__element" type="button"><span>#130</span></button>
               </section>
               <section className="step stage__step">
                 <h3 className="step__title">Шаг №2</h3>
@@ -38,30 +38,44 @@ class App extends Component {
                   <span>Время</span>
                   <b>00:00:00</b>
                 </p>
-                <button className="element step__element" type="button">#130</button>
+                <button className="element step__element" type="button"><span>#130</span></button>
               </section>
-              <footer className="stage__step-add">
+              <footer className="add-item">
                 <h2 className="visually-hidden">Добавление шага</h2>
-                <button type="button">Добавить шаг</button>
+                <button className="add-item__button" type="button"><span>Добавить шаг</span></button>
               </footer>
             </section>
           </section>
-
-          <section className="popup">
-            <h2 className="visually-hidden">Всплывающее окно</h2>
-            <div className="popup__overlay"/>
-            <form className="popup__form">
-              <label className="popup__label" htmlFor="popup__name">Название:</label>
-              <input className="popup__input-text" type="text" placeholder="Название шага.." id="popup__name"/>
-              <label className="popup__label" htmlFor="popup__employee">Ответственный:</label>
-              <input className="popup__input-text" type="text" placeholder="Иванов Иван.." id="popup__employee"/>
-              <label className="popup__label" htmlFor="popup__time">Время на выполнение:</label>
-              <input className="popup__input-text" type="text" placeholder="45:00" id="popup__time"/>
-              <button className="popup__button popup__button--cancel" type="button">Отмена</button>
-              <button className="popup__button popup__button--submit" type="submit">Сохранить</button>
-            </form>
-          </section>
         </div>
+        <section className="popup">
+          <h2 className="visually-hidden">Всплывающее окно</h2>
+          <div className="popup__overlay"/>
+          <form className="popup__form">
+            <div className="popup__wrapper">
+              <h2 className="popup__title">Редактировать шаг</h2>
+              <label className="popup__label" htmlFor="popup__name">Название:</label>
+              <input className="popup__input-text" required type="text" placeholder="Название шага.." id="popup__name"/>
+              <label className="popup__label" htmlFor="popup__employee">Ответственный:</label>
+              <input className="popup__input-text"
+                     required type="text"
+                     placeholder="Иванов Иван.."
+                     id="popup__employee"
+              />
+              <label className="popup__label" htmlFor="popup__time">Время на выполнение(ЧЧ:ММ):</label>
+              <input
+                className="popup__input-text"
+                required pattern="[0-9]{2}[:][0-9]{2}"
+                type="text"
+                placeholder="45:00"
+                id="popup__time"
+              />
+              <div className="popup__buttons">
+                <button className="popup__button popup__button--cancel" type="button">Отмена</button>
+                <button className="popup__button popup__button--submit" type="submit">Сохранить</button>
+              </div>
+            </div>
+          </form>
+        </section>
       </Fragment>
     );
   }
